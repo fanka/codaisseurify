7a7a737b7ff664e28b2186586a6f4107bd6dc06c
fanka <fanka@bk.ru>
2017-05-24 17:24:41 +0200

    Added artist:references and seeds

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,5 +4,10 @@ class SongsController < ApplicationController
   @songs = artist.songs
   end
 
+private
+
+def profile_params
+      params.require(:song).permit(:name, :artists_id)
+    end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Artist.destroy_all
+garou = Artist.create(name: "Garou", image_url: "https://upload.wikimedia.org/wikipedia/commons/0/03/Garou_avp_Iron_Man_3_Paris.jpg")
+obispo = Artist.create(name: "Pascal Obispo", image_url: "http://img.voi.pmdstatic.net/fit/http.3A.2F.2Fwww.2Evoici.2Efr.2Fvar.2Fvoi.2Fstorage.2Fimages.2Fbios-people.2Fpascal-obispo.2F8309185-865-fre-FR.2Fpascal-obispo.2Ejpg/380x285/quality/80/pascal-obispo.jpg")
+
+Song.create(name:"Je n'attendais que vous", artist: garou)
+Song.create(name:"Seul", artist: garou)
+Song.create(name:"Fan", artist: obispo)
+Song.create(name:"Lucie", artist: obispo)
