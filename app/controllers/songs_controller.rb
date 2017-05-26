71058b9fcff7bd4fee736756257d697ac3cb08b2
fanka <fanka@bk.ru>
2017-05-26 15:01:23 +0200

    Added New method to Songs controller, updated views

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,6 +4,20 @@ class SongsController < ApplicationController
   @songs = artist.songs
   end
 
+  def new
+    @song = Song.new
+   end
+
+
+  def create
+  @song = Song.new(song_params)
+    if @song.save
+      redirect_to @song
+    else
+      render "new"
+    end
+  end
+
 private
 
 def profile_params
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170524152206) do
+ActiveRecord::Schema.define(version: 20170526124048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20170524152206) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "artist_id"
+    t.text "lyrics"
     t.index ["artist_id"], name: "index_songs_on_artist_id"
   end
 
