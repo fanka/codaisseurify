eafe052f70651816b3c96ab7847b1a7a0078f17d
fanka <fanka@bk.ru>
2017-05-28 13:55:34 +0200

    Updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,15 @@
 Artist.destroy_all
 garou = Artist.create(name: "Garou", image_url: "https://fanparty.ru/fanclubs/garou/articles/79251/70541_tribune_garou.jpg")
 obispo = Artist.create(name: "Pascal Obispo", image_url: "https://www.lanuitmagazine.com/lanuitmag/wp-content/uploads/2016/10/11-16-pascal-obispo-dome-concert-marseille.jpg")
+fiori = Artist.create(name: "Patrick Fiori", image_url:"http://muz-lyrics.ru/photos/artists/5/22769/patrick-fiori-6.jpg")
+youn = Artist.create(name: "Michael Youn", image_url: "https://i.skyrock.net/0266/6600266/pics/102288216_small.jpg")
+
 
 Song.create(name:"Je n'attendais que vous", artist: garou)
 Song.create(name:"Seul", artist: garou)
 Song.create(name:"Fan", artist: obispo)
 Song.create(name:"Lucie", artist: obispo)
+Song.create(name:"Sans bruit", artist: fiori)
+Song.create(name:"Belle", artist: fiori)
+Song.create(name:"Fous ta cagoule!", artist: youn)
+Song.create(name:"Parle a ma main", artist: youn)
