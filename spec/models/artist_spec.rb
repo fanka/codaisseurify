c7a73eed18d5c583487f538fdf959c09c66abec9
fanka <fanka@bk.ru>
2017-05-28 13:10:25 +0200

    Added association tests

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,9 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Artist, type: :model do
-  describe "validations" do
-  it  { is_expected.to validate_presence_of(:name) }
 
+  describe "validations" do
+    it  { is_expected.to validate_presence_of(:name) }
   end
+
+  describe ".order_by_name" do
+   let!(:artist1) { create :artist, name: "Berty" }
+   let!(:artist2) { create :artist, name: "Alain" }
+   let!(:artist3) { create :artist, name: "Claude" }
+
+    it "returns a sorted array of artists by names" do
+     # note that they should not come out in the order that they were created
+     expect(Artist.order_by_name).to eq([artist2, artist1, artist3])
+    end
+  end
+
+    describe "associations with Songs" do
+          it  { is_expected.to have_many(:songs) }
+    end
+
+    describe "deletes all assotiated songs" do
+      let!(:artist1) { create :artist, name: "Berty" }
+      let!(:song1) { create :song, artist: artist1 }
+
+      it "deletes associated songs" do
+        expect { artist1.destroy }.to change(Song, :count).by(-1)
+      end
+    end
 
 end
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Song, type: :model do
   describe "validations" do
-  it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:name) }
   end
+
+  describe "associations with Artist" do
+    it  { is_expected.to belong_to(:artist) }
+  end
+  
 end
